14ab9995817be6b906260c44e65b22f2ad39f03f
egordenisov4-cmyk <egordenisov4@gmail.com>
2026-01-06 01:04:08 +0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 
-app = Flask(__name__)
+app = flask(__name__)
 app.secret_key = "secret123"  # обязательно для сессий
 
 
@@ -72,6 +72,7 @@ def logout():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
 
